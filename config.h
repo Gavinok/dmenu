8de4eb7bbbad4128b153ae272342747606e4169e
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-08-14 08:31:48 -0600

    added colorschemes support

--- config.h
+++ config.h
@@ -8,46 +8,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-static const char background[]  = "#111314";
-static const char foreground[]  = "#B7BBB7";
-
-
-static const char black1[]      = "#505050";
-static const char black2[]      = "#505050";
-                                          
-                                          
-static const char red1[]        = "#fb9fb1";
-static const char red2[]        = "#B04C50";
-                                          
-                                          
-static const char green1[]      = "#acc267";
-static const char green2[]      = "#acc267";
-                                          
-                                          
-static const char yellow1[]     = "#ddb26f";
-static const char yellow2[]     = "#E2995C";
-                                          
-                                          
-static const char blue1[]       = "#7D8FA3";
-static const char blue2[]       = "#66899D";
-                                          
-                                          
-static const char magenta1[]    = "#e1a3ee";
-static const char magenta2[]    = "#FF99FFf";
-                                          
-                                          
-static const char cyan1[]       = "#53cdbd";
-static const char cyan2[]       = "#527C77";
-                                          
-                                          
-static const char white1[]      = "#d0d0d0";
-static const char white2[]      = "#f5f5f5";
+#include "../colorschemes/dmenu/current.c"
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { blue1, background },
-	[SchemeSel] = { background, blue1},
-	[SchemeOut] = { blue1, foreground },
+	[SchemeNorm]= { color2, background },
+	[SchemeSel] = { background, color1},
+	[SchemeOut] = { color1, foreground },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
