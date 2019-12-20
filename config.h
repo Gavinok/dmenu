237a08bd9aed1326719584a70dc36e2d4de2fa18
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2019-12-20 16:29:09 -0700

    allowed for more configuration of colors

--- config.h
+++ config.h
@@ -12,9 +12,9 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm]= { color2, background },
-	[SchemeSel] = { background, color1},
-	[SchemeOut] = { color1, foreground },
+	[SchemeNorm]= { NORM, background },
+	[SchemeSel] = { background, SEL},
+	[SchemeOut] = { SEL, foreground },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
