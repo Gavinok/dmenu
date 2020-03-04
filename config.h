720d1edf5398c421809f5740fa503d69802d4ffe
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2020-03-04 11:57:22 -0700

    Changed default line number and fonts

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 
-static const char *fonts[]          = { "DejaVu Sans Mono:style=Regular:size=10", "FontAwesome:style=Regular:size=11"};
+static const char *fonts[]          = { "monospace:size=10", "FontAwesome:size=8" };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
 #include "../colorschemes/dmenu/current.h"
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { SEL, foreground },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 5;
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
